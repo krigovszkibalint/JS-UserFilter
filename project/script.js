df54eb72cb9af3f2e5c8fb842f330ad454ee6aca
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-04-24 05:01:21 +0200

    Number of users fetched

--- project/script.js
+++ project/script.js
@@ -7,7 +7,7 @@ getData()
 filter.addEventListener('input', (e) => filterData(e.target.value))
 
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
+    const res = await fetch('https://randomuser.me/api?results=500')
 
     const { results } = await res.json()
 
